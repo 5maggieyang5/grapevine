5cbe7492ca63ff62237b72b1954db4a394655d89
mag <mag.it.use@gmail.com>
2018-09-26 18:09:02 +0000

    added login component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'; // import the react-router-dom components
-import { Login } from './pages' // import our pages
 import SimpleMap  from './Maptst';
 import TopNav from './TopNav.jsx'
 import './styles/App.css'
 import Register from './Register.jsx'
 import Posts from './Home.jsx'
+import Login from './Login.jsx'
 
 
 const Main = () => (
