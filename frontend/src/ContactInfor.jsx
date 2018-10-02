a34269afef38f7b222119e9375b28952edecb76f
mag <mag.it.use@gmail.com>
2018-10-02 22:32:10 +0000

    added user email & phone number on the contact infor table

--- frontend/src/ContactInfor.jsx
+++ frontend/src/ContactInfor.jsx
@@ -5,24 +5,27 @@ class ContactInfor extends Component {
   render(){
 
     return (
-      <table>
-        <thead>
-          <tr>
-            <th className="tableLine">Username</th>
-            <th className="tableLine">Email:</th>
-            <th className="tableLine">Phone Number:</th>
-          </tr>
-        </thead>
-        <tbody>
-        {this.props.users.map((user) => (
-          <tr>
-            <td className="tableLine">{user.username}</td>
-            <td className="tableLine">{user.username}</td>
-            <td className="tableLine">{user.username}</td>
-          </tr>
-         ))}
-        </tbody>
-      </table>
+      <div>
+        <h1 id="contact_header"> Here is all the users' contact information:</h1>
+        <table id="contact_table">
+          <thead>
+            <tr>
+              <th className="tableLine">Username</th>
+              <th className="tableLine">Email:</th>
+              <th className="tableLine">Phone Number:</th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.props.users.map((user) => (
+            <tr>
+              <td className="tableLine">{user.username}</td>
+              <td className="tableLine">{user.email}</td>
+              <td className="tableLine">{user.phone_number}</td>
+            </tr>
+           ))}
+          </tbody>
+        </table>
+      </div>
     )
   }
 }
